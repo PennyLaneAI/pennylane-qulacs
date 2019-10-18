b778c7c1eea992065748f6a40fc54a3c28233df0
Steven Oud <soud@protonmail.com>
2019-10-18 13:01:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
+import re
+
 from setuptools import setup
 
 
+with open('./pennylane_qulacs/__init__.py') as f:
+    version, = re.findall('__version__ = \'(.*)\'', f.read())
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
+    name='PennyLane-Qulacs',
+    description='PennyLane plugin for Qulacs',
+    version=version,
     requirements=[
         'pennylane>=0.5.0',
-        'numpy'
+        'numpy',
+        'qulacs'
     ],
-    entry_points={'pennylane.plugins': [
-        'default.qulacs = pennylane_qulacs.qulacs_device:QulacsDevice'
-    ]}
+    entry_points={
+        'pennylane.plugins': [
+            'qulacs.simulator = pennylane_qulacs.qulacs_device:QulacsDevice'
+        ]
+    }
 )
