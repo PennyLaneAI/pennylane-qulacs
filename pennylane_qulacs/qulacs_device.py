b48182658f26d3f4f9ecf7d88affac4e1aefd312
Steven Oud <soud@protonmail.com>
2019-11-15 12:04:11 +0100

    Add tests and support more features

--- pennylane_qulacs/qulacs_device.py
+++ pennylane_qulacs/qulacs_device.py
@@ -1,10 +1,12 @@
 import numpy as np
 import functools
+import itertools
+from collections import OrderedDict
 
 from pennylane import Device, DeviceError
 
 from qulacs import Observable, QuantumCircuit, QuantumState
-from qulacs.gate import CNOT, RX, RY, RZ, X, Y, Z, H, U3, DenseMatrix
+import qulacs.gate as gate
 from qulacs.state import inner_product
 
 from . import __version__
@@ -17,10 +19,24 @@ except ImportError:
     GPU_SUPPORTED = False
 
 
-X_matrix = np.array([[0, 1], [1, 0]])
-Y_matrix = np.array([[0, -1j], [1j, 0]])
-Z_matrix = np.array([[1, 0], [0, -1]])
-H_matrix = np.array([[1, 1], [1, -1]])/np.sqrt(2)
+I = np.identity(2)
+X = np.array([[0, 1], [1, 0]])
+Y = np.array([[0, -1j], [1j, 0]])
+Z = np.array([[1, 0], [0, -1]])
+H = np.array([[1, 1], [1, -1]])/np.sqrt(2)
+
+rx = lambda theta: np.cos(theta / 2) * I + 1j * np.sin(-theta / 2) * X
+ry = lambda theta: np.cos(theta / 2) * I + 1j * np.sin(-theta / 2) * Y
+rz = lambda theta: np.cos(theta / 2) * I + 1j * np.sin(-theta / 2) * Z
+rot = lambda a, b, c: rz(c) @ (ry(b) @ rz(a))
+crz = lambda theta: np.array(
+    [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, np.exp(-1j * theta / 2), 0],
+        [0, 0, 0, np.exp(1j * theta / 2)],
+    ]
+)
 
 
 def hermitian(*args):
@@ -59,21 +75,32 @@ class QulacsDevice(Device):
     observables = {'PauliX', 'PauliY', 'PauliZ', 'Hermitian'}
 
     _operations_map = {
-        'CNOT': CNOT,
-        'RX': RX,
-        'RY': RY,
-        'RZ': RZ,
-        'Rot': U3,
+        'QubitStateVector': None,
+        'BasisState': None,
+        'QubitUnitary': None,
+        'CRZ': None,
+        'Rot': None,
+        'SWAP': gate.SWAP,
+        'CNOT': gate.CNOT,
+        'CZ': gate.CZ,
+        'S': gate.S,
+        'Sdg': gate.Sdag,
+        'T': gate.T,
+        'Tdg': gate.Tdag,
+        'RX': gate.RX,
+        'RY': gate.RY,
+        'RZ': gate.RZ,
+        'PauliX': gate.X,
+        'PauliY': gate.Y,
+        'PauliZ': gate.Z,
+        'Hadamard': gate.H
+    }
+    _observable_map = {
         'PauliX': X,
         'PauliY': Y,
         'PauliZ': Z,
-        'Hadamard': H
-    }
-    _observable_map = {
-        'PauliX': X_matrix,
-        'PauliY': Y_matrix,
-        'PauliZ': Z_matrix,
-        'Hadamard': H_matrix,
+        'Hadamard': H,
+        'Identity': I,
         'Hermitian': hermitian
     }
 
@@ -82,20 +109,65 @@ class QulacsDevice(Device):
 
         if gpu:
             if not GPU_SUPPORTED:
-                raise DeviceError('GPU not supported with installed version of qulacs.' +
-                                  ' Please install "qulacs-gpu" to use GPU simulation.')
+                raise DeviceError(
+                    'GPU not supported with installed version of qulacs. '
+                    'Please install "qulacs-gpu" to use GPU simulation.'
+                )
 
             self._state = QuantumStateGpu(wires)
         else:
             self._state = QuantumState(wires)
 
         self._circuit = QuantumCircuit(wires)
+        self._first_operation = True
 
     def apply(self, operation, wires, par):
+        if operation == 'BasisState' and not self._first_operation:
+            raise DeviceError(
+                'Operation {} cannot be used after other Operations have already been applied '
+                'on a {} device.'.format(operation, self.short_name)
+            )
+
+        self._first_operation = False
+
+        if operation != 'QubitUnitary':
+            wires = list(reversed(wires))
+
         if operation == 'QubitStateVector':
             if len(par[0]) != 2**len(wires):
                 raise ValueError('State vector must be of length 2**wires.')
+
             self._state.load(par[0])
+
+            return
+        elif operation == 'BasisState':
+            if len(par[0]) != len(wires):
+                raise ValueError('Basis state must prepare all qubits.')
+
+            basis_state = 0
+            for bit in par[0]:
+                basis_state = (basis_state << 1) | bit
+
+            self._state.set_computational_basis(basis_state)
+
+            return
+        elif operation == 'QubitUnitary':
+            if len(par[0]) != 2 ** len(wires):
+                raise ValueError('Unitary matrix must be of shape (2**wires, 2**wires).')
+
+            unitary_gate = gate.DenseMatrix(wires, par[0])
+            self._circuit.add_gate(unitary_gate)
+
+            return
+        elif operation == 'Rot':
+            rot_gate = gate.DenseMatrix(wires, rot(par[0], par[1], par[2]))
+            self._circuit.add_gate(rot_gate)
+
+            return
+        elif operation == 'CRZ':
+            crz_gate = gate.DenseMatrix(wires, crz(par[0]))
+            self._circuit.add_gate(crz_gate)
+
             return
 
         mapped_operation = self._operations_map[operation]
@@ -103,9 +175,7 @@ class QulacsDevice(Device):
 
     @property
     def state(self):
-        state = self._state.get_vector()
-
-        return state.reshape([2] * self.num_wires).T.flatten()
+        return self._state.get_vector()
 
     def pre_measure(self):
         self._circuit.update_quantum_state(self._state)
@@ -115,16 +185,24 @@ class QulacsDevice(Device):
 
         if isinstance(observable, list):
             A = self._get_tensor_operator_matrix(observable, par)
-            wires = [item for sublist in wires for item in sublist]
+            wires = [item for sublist in reversed(wires) for item in sublist]
         else:
             A = self._get_operator_matrix(observable, par)
 
-        dense_gate = DenseMatrix(wires, A)
+        wires = list(reversed(wires))
+
+        dense_gate = gate.DenseMatrix(wires, A)
         dense_gate.update_quantum_state(self._state)
 
         expectation = inner_product(bra, self._state)
 
         return expectation.real
+
+    def probabilities(self):
+        states = itertools.product(range(2), repeat=self.num_wires)
+        probs = np.abs(self.state)**2
+
+        return OrderedDict(zip(states, probs))
 
     def reset(self):
         self._state.set_zero_state()
