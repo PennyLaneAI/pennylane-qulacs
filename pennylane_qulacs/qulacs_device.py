9c373b4931b53a6887bf55f1959bfe29909e8b0c
Maria Schuld <mariaschuld@gmail.com>
2020-06-25 08:13:12 +0200

    Update pennylane_qulacs/qulacs_device.py

--- pennylane_qulacs/qulacs_device.py
+++ pennylane_qulacs/qulacs_device.py
@@ -196,7 +196,7 @@ class QulacsDevice(QubitDevice):
                     raise ValueError("State vector must be of length 2**wires.")
                 if not np.isclose(np.linalg.norm(input_state, 2), 1.0, atol=tolerance):
                     raise ValueError("Sum of amplitudes-squared does not equal one.")
-                # call qulac"s state initialization
+                # call qulacs' state initialization
                 self._state.load(input_state)
 
             elif op.name in ["BasisState", "BasisState.inv"]:
