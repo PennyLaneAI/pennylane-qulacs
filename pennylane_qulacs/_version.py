e00348a07cf68cd987402933c0a14e58f5fade48
Jay Soni <jbsoni@uwaterloo.ca>
2022-06-14 17:48:49 -0400

    Added version bump code and _version.py file

--- pennylane_qulacs/_version.py
+++ pennylane_qulacs/_version.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Top level Pennylane-qulacs plugin"""
+"""Version information.
+   Version number (major.minor.patch[-label])
+"""
+
+__version__ = "0.17.0-dev"
